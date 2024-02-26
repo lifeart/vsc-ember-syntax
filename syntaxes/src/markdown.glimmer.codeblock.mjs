539aa5dbbf7939e878c7fabce939f06dc0805b0d
spuxx1701 <hi@spuxx.dev>
2024-02-26 16:00:04 +0100

    refactor: Use inline regular expressions

--- syntaxes/src/markdown.glimmer.codeblock.mjs
+++ syntaxes/src/markdown.glimmer.codeblock.mjs
@@ -1,21 +1,8 @@
-function regexes(extension) {
-  return {
-    fencedCodeBlock: {
-      begin: `(^|\\G)(\\s*)(\\\`{3,}|~{3,})\\s*(?i:(${extension})(\\s+[^\`~]*)?$)`,
-      end: '(^|\\G)(\\2|\\s{0,3})(\\3)\\s*$',
-    },
-    embeddedBlock: {
-      begin: '(^|\\G)(\\s*)(.*)',
-      while: '(^|\\G)(?!\\s*([`~]{3,})\\s*$)',
-    },
-  };
-}
-
 function codeblock(extension) {
   return {
     name: 'markup.fenced_code.block.markdown',
-    begin: regexes(extension).fencedCodeBlock.begin,
-    end: regexes(extension).fencedCodeBlock.end,
+    begin: `(^|\\G)(\\s*)(\\\`{3,}|~{3,})\\s*(?i:(${extension})(\\s+[^\`~]*)?$)`,
+    end: '(^|\\G)(\\2|\\s{0,3})(\\3)\\s*$',
     beginCaptures: {
       3: {
         name: 'punctuation.definition.markdown',
@@ -34,8 +21,8 @@ function codeblock(extension) {
     },
     patterns: [
       {
-        begin: regexes(extension).embeddedBlock.begin,
-        while: regexes(extension).embeddedBlock.while,
+        begin: '(^|\\G)(\\s*)(.*)',
+        while: '(^|\\G)(?!\\s*([`~]{3,})\\s*$)',
         contentName: `meta.embedded.block.${extension}`,
         patterns: [
           {
